3dbf373500862151a4252e50644e13a4daf35201
Eatowl <csnelcov@gmail.com>
2017-01-20 20:52:47 +0700

    update

--- main.c
+++ main.c
@@ -4,36 +4,36 @@
 #include <stdio.h>
 
 int main() {
-	pid_t f_pid;
-	f_pid = getpid();
-	printf("parent pid -- %d\n", f_pid);
+	pid_t p_pid;
+	p_pid = getpid();
+	printf("parent pid -- %d\n", p_pid);
 	if (fork() == 0) {
-		pid_t pid1;
-		pid1 = getpid();
-		printf("parent %d <- child №1 %d\n", f_pid, pid1);
+		pid_t new_pid;
+		new_pid = getpid();
+		printf("parent %d <- child №1 %d\n", p_pid, new_pid);
 		if (fork() == 0) {
 			pid_t new_child;
 			new_child = getpid();
-			printf("parent %d <- child №1 %d\n", pid1, new_child);
+			printf("parent %d <- child №1 %d\n", new_pid, new_child);
 			return 2;
 		}
 		wait(0);
 		return 1;
 	}
 	if (fork() == 0) {
-		pid_t new_pid;
-		new_pid = getpid();
-		printf("parent %d <- child №2 %d\n", f_pid, new_pid);
+		pid_t new_pid1;
+		new_pid1 = getpid();
+		printf("parent %d <- child №2 %d\n", p_pid, new_pid1);
 		if (fork() == 0) {
 			pid_t new_child1;
 			new_child1 = getpid();
-			printf("parent %d <- child №1 %d\n", new_pid, new_child1);
+			printf("parent %d <- child №1 %d\n", new_pid1, new_child1);
 			return 2;
 		}
 		if (fork() == 0) {
 			pid_t new_child2;
 			new_child2 = getpid();
-			printf("parent %d <- child №2 %d\n", new_pid, new_child2);
+			printf("parent %d <- child №2 %d\n", new_pid1, new_child2);
 			return 2;
 		}
 		wait(0);
